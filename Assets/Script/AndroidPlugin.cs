d296f744b210ec6f6126074ae2d174b42d5c7e48
hayuart <hayu@artygenspace.com>
2024-12-18 18:55:14 +0900

    apk 빌드 필요 xml고치는중

---
+++ Assets/Script/AndroidPlugin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AndroidPlugin : MonoBehaviour
+{
+    void Start()
+    {
+        using (AndroidJavaClass myClass = new AndroidJavaClass("com.example.MyJavaClass"))
+        {
+            string result = myClass.CallStatic<string>("getStringFromJava");
+            Debug.Log("Received from Java: " + result);
+        }
+    }
+
+    public void CallJavaMethod()
+    {
+        using (AndroidJavaClass myClass = new AndroidJavaClass("com.example.MyJavaClass"))
+        {
+            using (AndroidJavaObject myObject = myClass.CallStatic<AndroidJavaObject>("getInstance"))
+            {
+                int value = myObject.Call<int>("getIntValue");
+                Debug.Log("Integer value from Java: " + value);
+            }
+        }
+    }
+}
